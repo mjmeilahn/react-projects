aecfb346c1253c843276ed7857134ecd7efeb33e
mjmeilahn <mjmeilahn@gmail.com>
2021-12-06 11:57:23 -0800

    Adding new components, global state, one-way data flow.

--- 01-basics/src/App.js
+++ 01-basics/src/App.js
@@ -2,10 +2,34 @@
 import ExpenseItem from "./components/ExpenseItem"
 
 function App() {
+
+  // GLOBAL STATE
+  // IN THE FUTURE YOU'D CONNECT TO A DATABASE OR REDUX
+  const expenses = [
+    {
+      title: 'Car Insurance',
+      amount: 297.35,
+      date: new Date(2021, 3, 4)
+    },
+    {
+      title: 'Electric Bill',
+      amount: 50.13,
+      date: new Date(2021, 2, 5)
+    },
+    {
+      title: 'Phone Bill',
+      amount: 27.00,
+      date: new Date(2021, 5, 6)
+    }
+  ]
+
+  // JSX IS ALWAYS RENDERED WITH PARENTHESIS OR AN ARROW FUNCTION
   return (
     <div>
       <h2>Hello World</h2>
-      <ExpenseItem/>
+      {expenses.map((e, i) => (
+        <ExpenseItem key={i} title={e.title} amount={e.amount} date={e.date}/>
+      ))}
     </div>
   )
 }
