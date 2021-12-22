482dc1e38bbc68c8d5cedc55095096a73c26708a
mjmeilahn <mjmeilahn@gmail.com>
2021-12-22 14:11:49 -0800

    Removing pesky semi-colons

--- 07-optimization-techniques/src/App.js
+++ 07-optimization-techniques/src/App.js
@@ -1,13 +1,12 @@
-import React from 'react';
 
-import './App.css';
+import './App.css'
 
 function App() {
   return (
     <div className="app">
       <h1>Hi there!</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
