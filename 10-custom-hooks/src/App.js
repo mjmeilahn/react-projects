d1f453a35f84c942c9e5a18a5039003079308020
mjmeilahn <mjmeilahn@gmail.com>
2021-12-29 11:36:26 -0800

    Finish custom hooks example

--- 10-custom-hooks/src/App.js
+++ 10-custom-hooks/src/App.js
@@ -3,6 +3,14 @@ import React from 'react'
 import BackwardCounter from './components/BackwardCounter'
 import ForwardCounter from './components/ForwardCounter'
 
+
+// CUSTOM HOOKS MUST FOLLOW A STRICT NAMING CONVENTION
+// ACCORDING TO REACT. ALWAYS START WITH "use"
+// AT THE BEGINNING OF THE FUNCTION.
+
+// THE CUSTOM HOOK CAN BE LOCATED ANYWHERE BUT COMMON CONVENTION
+// IS A DEDICATED FOLDER NEAR THE ROOT OF THE "src" FOLDER.
+
 function App() {
   return (
     <React.Fragment>
