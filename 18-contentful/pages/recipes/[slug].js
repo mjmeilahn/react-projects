1131d09f1b8f51bcc3c9b71d4ac1480a02cfd859
mjmeilahn <mjmeilahn@gmail.com>
2022-04-29 15:37:47 -0700

    Receives data from Contentful Headless CMS, need to finish example

--- 18-contentful/pages/recipes/[slug].js
+++ 18-contentful/pages/recipes/[slug].js
@@ -1,5 +1,44 @@
 
-export default function RecipeDetails() {
+import { createClient } from 'contentful'
+
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+})
+
+export async function getStaticPaths() {
+  const res = await client.getEntries({ content_type: 'recipe' })
+
+  const paths = res.items.map(item => {
+    return {
+      params: {
+        slug: item.fields.slug
+      }
+    }
+  })
+
+  return {
+    paths,
+    fallback: false // IF TRAFFIC HITS A URL WE HAVE NOT MAPPED IN PATHS, SEND THEM TO A 404 PAGE.
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const { items } = await client.getEntries({
+    content_type: 'recipe',
+    'fields.slug': params.slug
+  })
+
+  return {
+    props: {
+      recipe: items[0]
+    }
+  }
+}
+
+export default function RecipeDetails({ recipe }) {
+  console.log(recipe)
+
   return (
     <div>
       Recipe Details
